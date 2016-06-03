948453a0c3cdc23c8cf50585736ae99fa9d32512
David Podhola <david.podhola@naseukoly.cz>
2016-06-03 20:32:28 +0200

    generated empty to be buildable

--- src/genit/generated/generated_fake_data.fs
+++ src/genit/generated/generated_fake_data.fs
@@ -4,44 +4,4 @@ open generated_types
 open generated_data_access
 open helper_general
 
-let fake_order () =
-  let cityStateZip = randomItem citiesSatesZips
-  {
-    OrderID = -1L 
-    Name = (randomItem firstNames) + " " + (randomItem lastNames) 
-    Food = randomItems 6 words 
-    Drinks = randomItems 6 words 
-    Tip = random.Next(100) |> double 
-    Notes = randomItems 40 words 
-    DeliveryDate = System.DateTime.Now 
-    PhoneNumber = sprintf "%i-%i-%i" (random.Next(200,800)) (random.Next(200,800)) (random.Next(2000,8000)) 
-    Address = (string (random.Next(100,9999))) + " " + (randomItem streetNames) + " " + (randomItem streetNameSuffixes) 
-    City = cityStateZip.City 
-    State = cityStateZip.State 
-    Zip = cityStateZip.Zip 
-    FreeSoda = 1s 
-  }
-
-let fake_many_order number =
-  [| 1..number |]
-  |> Array.map (fun _ -> fake_order ()) //no parallel cause of RNG
-  |> Array.Parallel.map insert_order
-  |> ignore
- 
- 
-let fake_reserveration () =
-  {
-    ReserverationID = -1L 
-    Name = (randomItem firstNames) + " " + (randomItem lastNames) 
-    Date = System.DateTime.Now 
-    PhoneNumber = sprintf "%i-%i-%i" (random.Next(200,800)) (random.Next(200,800)) (random.Next(2000,8000)) 
-  }
-
-let fake_many_reserveration number =
-  [| 1..number |]
-  |> Array.map (fun _ -> fake_reserveration ()) //no parallel cause of RNG
-  |> Array.Parallel.map insert_reserveration
-  |> ignore
- 
- 
   
--- src/genit/generated/generated_validation.fs
+++ src/genit/generated/generated_validation.fs
@@ -3,41 +3,4 @@ module generated_validation
 open generated_forms
 open validators
 
-let validation_registerForm (registerForm : RegisterForm) =
-  [
-    validate_required "First Name" registerForm.FirstName
-    validate_required "Last Name" registerForm.LastName
-    validate_email "Email" registerForm.Email
-    validate_required "Email" registerForm.Email
-    validate_password "Password" registerForm.Password
-    validate_required "Password" registerForm.Password
-    validate_password "Confirm Password" registerForm.ConfirmPassword
-    validate_equal "Password" "Confirm Password" registerForm.Password registerForm.ConfirmPassword
-    validate_required "Confirm Password" registerForm.ConfirmPassword
-  ] |> List.choose id
-  
-let validation_loginForm (loginForm : LoginForm) =
-  [
-    validate_email "Email" loginForm.Email
-    validate_required "Email" loginForm.Email
-    validate_password "Password" loginForm.Password
-    validate_required "Password" loginForm.Password
-  ] |> List.choose id
-  
-let validation_orderForm (orderForm : OrderForm) =
-  [
-    validate_required "Name" orderForm.Name
-    validate_double "Tip" orderForm.Tip
-    validate_datetime "Delivery Date" orderForm.DeliveryDate
-    validate_required "Delivery Date" orderForm.DeliveryDate
-    validate_required "Phone Number" orderForm.PhoneNumber
-  ] |> List.choose id
-  
-let validation_reserverationForm (reserverationForm : ReserverationForm) =
-  [
-    validate_required "Name" reserverationForm.Name
-    validate_datetime "Date" reserverationForm.Date
-    validate_required "Date" reserverationForm.Date
-    validate_required "Phone Number" reserverationForm.PhoneNumber
-  ] |> List.choose id
   
